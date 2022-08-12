cae4a88b3dae8a7082c9a2f6ede0ea4b6d45e716
Steve Cochrane <steve@stevecochrane.com>
2022-08-11 22:42:33 -0500

    Remove unnecessary setIgnoresDrawOffset call

--- source/speed.lua
+++ source/speed.lua
@@ -1,14 +1,13 @@
 local gfx <const> = playdate.graphics
 
-class('Speed').extends(playdate.graphics.sprite)
+class("Speed").extends(playdate.graphics.sprite)
 
 function Speed:init()
 	Speed.super.init(self)
 
 	self.speed = 0
 
-	self:setZIndex(900)
-	self:setIgnoresDrawOffset(true)
+	self:setZIndex(1)
 	self:setCenter(0, 0)
 	self:setSize(40, 20)
 	self:moveTo(40, 40)
