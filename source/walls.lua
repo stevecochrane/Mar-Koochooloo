d95d4177bee2d2c4f4e6ca59889354bab3481e41
Steve Cochrane <steve@stevecochrane.com>
2022-09-02 15:54:07 -0500

    Add radio buttons to Walls

--- source/walls.lua
+++ source/walls.lua
@@ -1,6 +1,8 @@
 local gfx <const> = playdate.graphics
 
 local appleImage = gfx.image.new("images/apple")
+local radioNotSelectedImage = gfx.image.new("images/radio-not-selected")
+local radioSelectedImage = gfx.image.new("images/radio-selected")
 
 class("Walls").extends(playdate.graphics.sprite)
 
@@ -8,6 +10,9 @@ function Walls:init()
 	Walls.super.init(self)
 
 	self.appleSprite = nil
+	self.enabled = true
+	self.radioYesSprite = nil
+	self.radioNoSprite = nil
 	self.selected = false
 	self.speed = 0
 
@@ -15,10 +20,20 @@ function Walls:init()
 	self:setCenter(0, 0)
 	self:setSize(272, 20)
 	self:moveTo(72, 112)
+
+	self.radioYesSprite = gfx.sprite.new(radioSelectedImage)
+	self.radioYesSprite:setCenter(0, 0)
+	self.radioYesSprite:moveTo(self.x + 138, self.y)
+	self.radioYesSprite:add()
+
+	self.radioNoSprite = gfx.sprite.new(radioNotSelectedImage)
+	self.radioNoSprite:setCenter(0, 0)
+	self.radioNoSprite:moveTo(self.x + 197, self.y)
+	self.radioNoSprite:add()
 end
 
-function Walls:setSpeed(newSpeed)
-	self.speed = newSpeed
+function Walls:toggle()
+	self.enabled = not self.enabled
 	self:markDirty()
 end
 
@@ -46,6 +61,10 @@ function Walls:draw()
 
 	-- Draw "Speed" label text
 	gfx.drawText("Walls", 24, 0)
+
+	-- Draw "Yes" and "No" label text
+	gfx.drawText("Yes", 156, 0)
+	gfx.drawText("No", 215, 0)
 
 	gfx.setImageDrawMode(gfx.kDrawModeCopy)
 end
