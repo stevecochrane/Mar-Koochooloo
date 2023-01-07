f94c3ad68ed52ffc3e1347f6101f8008585f1794
Steve Cochrane <steve@stevecochrane.com>
2023-01-06 22:39:53 -0600

    Expand walls tilemap to two tile images

--- source/tilemaps/walls.tsx
+++ source/tilemaps/walls.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Walls" tilewidth="16" tileheight="16" tilecount="1" columns="1">
- <image source="walls-sprite.png" trans="000000" width="16" height="16"/>
+<tileset version="1.9" tiledversion="1.9.2" name="walls" tilewidth="16" tileheight="16" tilecount="2" columns="2">
+ <image source="walls-sprite.png" width="32" height="16"/>
 </tileset>
