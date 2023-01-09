f8d131a49d5f5c3b929df75c42d09abec7fff884
Steve Cochrane <steve@stevecochrane.com>
2023-01-08 22:11:09 -0600

    Add tilemap class with JSON loader function

---
+++ source/tilemap.lua
@@ -0,0 +1,24 @@
+function loadJsonData(levelJsonFilePath)
+	local levelData = nil
+	local file = playdate.file.open(levelJsonFilePath)
+
+	if file then
+		local fileSize = playdate.file.getSize(levelJsonFilePath)
+		levelData = file:read(fileSize)
+		file:close()
+
+		if not levelData then
+			print('ERROR LOADING DATA for ' .. path)
+			return nil
+		end
+	end
+
+	local jsonTable = json.decode(levelData)
+
+	if not jsonTable then
+		print('ERROR PARSING JSON DATA for ' .. levelPath)
+		return nil
+	end
+
+	return jsonTable
+end
