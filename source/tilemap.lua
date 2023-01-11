77e5578650ea0ce58e2341446c6b6b276f6f563c
Steve Cochrane <steve@stevecochrane.com>
2023-01-10 22:28:39 -0600

    Add getWallLocations function to tilemap

--- source/tilemap.lua
+++ source/tilemap.lua
@@ -22,3 +22,45 @@ function loadLevelJsonData(levelJsonFilePath)
 
 	return jsonTable
 end
+
+function getWallLocations(levelData)
+	local layers = levelData.layers
+	local wallsLayer = nil
+	local wallLocations = {}
+
+	for i = 1, #layers do
+		if layers[i].name == "walls" then
+			wallsLayer = layers[i]
+		end
+	end
+
+	if not wallsLayer then
+		print('ERROR LOCATING WALLS LAYER IN LEVEL DATA')
+		return nil
+	end
+
+	local wallsData = wallsLayer.data
+	local rows = wallsLayer.height
+	local columns = wallsLayer.width
+
+	-- TODO: Get these from the tilemap later
+	local tileHeight = 16
+	local tileWidth = 16
+
+	-- TODO: Iterate from 0 rather than from 1?
+	for row = 1, rows do
+		for column = 1, columns do
+			local index = column + ((row - 1) * columns)
+			if wallsData[index] == 2 then
+				local wallLocation = {(column - 1) * tileWidth, (row - 1) * tileHeight}
+				if row == 2 then
+					print('X' .. (column - 1) * tileWidth)
+					print('Y' .. (row - 1) * tileHeight)
+				end
+				table.insert(wallLocations, 1, wallLocation)
+			end
+		end
+	end
+
+	return wallLocations
+end
