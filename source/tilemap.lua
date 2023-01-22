6e45895be34d4054b4dddb8ca1c99423568122ae
Steve Cochrane <steve@stevecochrane.com>
2023-01-21 20:58:47 -0600

    Add logic for getting snake spawn location from level data

--- source/tilemap.lua
+++ source/tilemap.lua
@@ -1,5 +1,6 @@
 Tilemap = {}
 
+-- TODO: getWallsLayer and getSnakeLayer are very similar. Combine into one function?
 local function getWallsLayer(levelData)
 	local layers = levelData.layers
 	local wallsLayer = nil
@@ -11,11 +12,45 @@ local function getWallsLayer(levelData)
 	end
 
 	if not wallsLayer then
-		print('ERROR LOCATING WALLS LAYER IN LEVEL DATA')
+		print("ERROR LOCATING WALLS LAYER IN LEVEL DATA")
 		return nil
 	end
 
 	return wallsLayer
+end
+
+local function getSnakeLayer(levelData)
+	local layers = levelData.layers
+	local snakeLayer = nil
+
+	for i = 1, #layers do
+		if layers[i].name == "snake" then
+			snakeLayer = layers[i]
+		end
+	end
+
+	if not snakeLayer then
+		print("ERROR LOCATING SNAKE LAYER IN LEVEL DATA")
+	end
+
+	return snakeLayer
+end
+
+local function getSnakeSpawnObject(snakeLayer)
+	local objects = snakeLayer.objects
+	local spawnObject = nil
+
+	for i = 1, #objects do
+		if objects[i].name == "spawn" then
+			spawnObject = objects[i]
+		end
+	end
+
+	if not spawnObject then
+		print("ERROR LOADING SPAWN OBJECT FROM SNAKE LAYER IN LEVEL DATA")
+	end
+
+	return spawnObject
 end
 
 local function getWallsTileset(levelData)
@@ -29,7 +64,7 @@ local function getWallsTileset(levelData)
 	end
 
 	if not wallsTileset then
-		print('ERROR LOCATING WALLS TILESET IN LEVEL DATA')
+		print("ERROR LOCATING WALLS TILESET IN LEVEL DATA")
 		return nil
 	end
 
@@ -46,7 +81,7 @@ function Tilemap:loadLevelJsonData(levelJsonFilePath)
 		file:close()
 
 		if not levelData then
-			print('ERROR LOADING DATA for ' .. path)
+			print("ERROR LOADING DATA for " .. path)
 			return nil
 		end
 	end
@@ -54,7 +89,7 @@ function Tilemap:loadLevelJsonData(levelJsonFilePath)
 	local jsonTable = json.decode(levelData)
 
 	if not jsonTable then
-		print('ERROR PARSING JSON DATA for ' .. levelPath)
+		print("ERROR PARSING JSON DATA for " .. levelPath)
 		return nil
 	end
 
@@ -90,4 +125,16 @@ function Tilemap:getWallLocations(levelData)
 	end
 
 	return wallLocations
+end
+
+function Tilemap:getSnakeSpawnLocation(levelData)
+	local snakeLayer = getSnakeLayer(levelData)
+	local snakeSpawnObject = getSnakeSpawnObject(snakeLayer)
+
+	if not snakeSpawnObject.x or not snakeSpawnObject.y then
+		print("ERROR LOADING X AND Y COORDINATES FOR SNAKE SPAWN LOCATION")
+		return nil
+	end
+
+	return {snakeSpawnObject.x, snakeSpawnObject.y}
 end
