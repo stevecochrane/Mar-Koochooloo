ccf7209c409a1ab832ea5c00db7c45c65a60ebfe
Steve Cochrane <steve@stevecochrane.com>
2023-03-09 22:22:55 -0600

    Allow pressing B to go back to Title from Credits

--- source/stateCredits.lua
+++ source/stateCredits.lua
@@ -8,7 +8,7 @@ function StateCredits:switch()
 end
 
 function StateCredits:update()
-	if stateSwitchInProgress == false and playdate.buttonJustPressed(playdate.kButtonA) then
+	if stateSwitchInProgress == false and (playdate.buttonJustPressed(playdate.kButtonA) or playdate.buttonJustPressed(playdate.kButtonB)) then
 		gfx.sprite.removeAll()
 		stateSwitchInProgress = true
 		playdate.timer.performAfterDelay(stateSwitchPauseDuration, startGame)
