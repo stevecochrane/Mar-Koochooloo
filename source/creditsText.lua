a65a12b5396246893647d2705223005cca818e18
Steve Cochrane <steve@stevecochrane.com>
2023-03-20 21:07:05 -0500

    Use Roobert fonts for Credits text

--- source/creditsText.lua
+++ source/creditsText.lua
@@ -10,34 +10,31 @@ function CreditsText:init()
 end
 
 function CreditsText:draw()
-	local Asheville14Bold <const> = gfx.font.new('Fonts/Asheville-Sans-14-Bold')
-	local Asheville14Light <const> = gfx.font.new('Fonts/Asheville-Sans-14-Light')
+	local Roobert11Medium <const> = gfx.font.new('Fonts/Roobert-11-Medium')
+	local Roobert10Bold <const> = gfx.font.new('Fonts/Roobert-10-Bold')
 
-	local leftMargin = 16
-	local rowHeight = 16
+	local leftMargin = 20
+	local rowHeight = 20
 
 	gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
 
-	gfx.setFont(Asheville14Bold)
-	gfx.drawText("For Darya with love", leftMargin, rowHeight * 1)
+	gfx.setFont(Roobert11Medium)
+	gfx.drawText("For Darya, with love", leftMargin, rowHeight * 1)
 
-	gfx.setFont(Asheville14Light)
-	gfx.drawText("Design, Programming, Art, Sound Effects", leftMargin, rowHeight * 3)
-	gfx.setFont(Asheville14Bold)
+	gfx.setFont(Roobert10Bold)
+	gfx.drawText("Design, Programming, Graphics", leftMargin, rowHeight * 3)
+	gfx.setFont(Roobert11Medium)
 	gfx.drawText("Steve Cochrane", leftMargin, rowHeight * 4)
 
-	gfx.setFont(Asheville14Light)
+	gfx.setFont(Roobert10Bold)
 	gfx.drawText("Music, Sound Effects", leftMargin, rowHeight * 6)
-	gfx.setFont(Asheville14Bold)
+	gfx.setFont(Roobert11Medium)
 	gfx.drawText("Nik Son", leftMargin, rowHeight * 7)
 
-	gfx.setFont(Asheville14Light)
+	gfx.setFont(Roobert10Bold)
 	gfx.drawText("Playtesting, Feedback", leftMargin, rowHeight * 9)
-	gfx.setFont(Asheville14Bold)
+	gfx.setFont(Roobert11Medium)
 	gfx.drawText("Nazgol Bagheri, Darya Cochrane", leftMargin, rowHeight * 10)
-
-	gfx.setFont(Asheville14Bold)
-	gfx.drawText("Thank you for playing!", leftMargin, rowHeight * 12)
 
 	-- Reset to defaults so this doesn't change other areas of the game
 	gfx.setFont(Asheville14Light)
