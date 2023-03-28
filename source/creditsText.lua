938ff026e2e9ec2423ab50a91cf8a395442fb534
Steve Cochrane <steve@stevecochrane.com>
2023-03-27 21:30:29 -0500

    Improve spacing for Credits text

--- source/creditsText.lua
+++ source/creditsText.lua
@@ -12,29 +12,29 @@ end
 function CreditsText:draw()
 	local Roobert10Bold <const> = gfx.font.new('Fonts/Roobert-10-Bold')
 
-	local leftMargin = 20
+	local margin = 10
 	local rowHeight = 20
 
 	gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
 
-	gfx.drawText("For Darya, with love", leftMargin, rowHeight * 1)
+	gfx.drawText("For Darya, with love", margin, margin)
 
 	gfx.setFont(Roobert10Bold)
-	gfx.drawText("Design, Programming, Graphics", leftMargin, rowHeight * 3)
+	gfx.drawText("Design, Programming, Graphics", margin, margin + rowHeight * 2)
 	gfx.setFont(Roobert11Medium)
-	gfx.drawText("Steve Cochrane", leftMargin, rowHeight * 4)
+	gfx.drawText("Steve Cochrane", margin, margin + rowHeight * 3)
 
 	gfx.setFont(Roobert10Bold)
-	gfx.drawText("Music, Sound Effects", leftMargin, rowHeight * 6)
+	gfx.drawText("Music, Sound Effects", margin, margin + rowHeight * 5)
 	gfx.setFont(Roobert11Medium)
-	gfx.drawText("Nik Son", leftMargin, rowHeight * 7)
+	gfx.drawText("Nik Son", margin, margin + rowHeight * 6)
 
 	gfx.setFont(Roobert10Bold)
-	gfx.drawText("Playtesting, Feedback", leftMargin, rowHeight * 9)
+	gfx.drawText("Playtesting, Feedback", margin, margin + rowHeight * 8)
 	gfx.setFont(Roobert11Medium)
-	gfx.drawText("Nazgol Bagheri, Darya Cochrane", leftMargin, rowHeight * 10)
+	gfx.drawText("Nazgol Bagheri, Darya Cochrane", margin, margin + rowHeight * 9)
 
-	gfx.drawTextAligned("Ⓐ", 380, rowHeight * 10, kTextAlignment.right)
+	gfx.drawTextAligned("Ⓐ", 390, margin + rowHeight * 10, kTextAlignment.right)
 
 	gfx.setImageDrawMode(gfx.kDrawModeCopy)
 end
