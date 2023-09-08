e31940fc69223e42a9559403890e7e316c977223
Steve Cochrane <steve@stevecochrane.com>
2023-09-07 21:49:44 -0500

    Segment Level Clear images by mode

--- source/nextLevelState.lua
+++ source/nextLevelState.lua
@@ -9,6 +9,15 @@ local level6ClearImage = gfx.image.new("images/level-6-clear")
 local level7ClearImage = gfx.image.new("images/level-7-clear")
 local level8ClearImage = gfx.image.new("images/level-8-clear")
 
+local level1ClearGentleImage = gfx.image.new("images/level-1-clear-gentle")
+local level2ClearGentleImage = gfx.image.new("images/level-2-clear-gentle")
+local level3ClearGentleImage = gfx.image.new("images/level-3-clear-gentle")
+local level4ClearGentleImage = gfx.image.new("images/level-4-clear-gentle")
+local level5ClearGentleImage = gfx.image.new("images/level-5-clear-gentle")
+local level6ClearGentleImage = gfx.image.new("images/level-6-clear-gentle")
+local level7ClearGentleImage = gfx.image.new("images/level-7-clear-gentle")
+local level8ClearGentleImage = gfx.image.new("images/level-8-clear-gentle")
+
 NextLevelState = {}
 
 function NextLevelState:switch()
@@ -19,22 +28,42 @@ function NextLevelState:switch()
 
 	local levelClearSprite = nil
 
-	if currentLevel == 1 then
-		levelClearSprite = gfx.sprite.new(level1ClearImage)
-	elseif currentLevel == 2 then
-		levelClearSprite = gfx.sprite.new(level2ClearImage)
-	elseif currentLevel == 3 then
-		levelClearSprite = gfx.sprite.new(level3ClearImage)
-	elseif currentLevel == 4 then
-		levelClearSprite = gfx.sprite.new(level4ClearImage)
-	elseif currentLevel == 5 then
-		levelClearSprite = gfx.sprite.new(level5ClearImage)
-	elseif currentLevel == 6 then
-		levelClearSprite = gfx.sprite.new(level6ClearImage)
-	elseif currentLevel == 7 then
-		levelClearSprite = gfx.sprite.new(level7ClearImage)
-	elseif currentLevel == 8 then
-		levelClearSprite = gfx.sprite.new(level8ClearImage)
+	if control == "classic" then
+		if currentLevel == 1 then
+			levelClearSprite = gfx.sprite.new(level1ClearImage)
+		elseif currentLevel == 2 then
+			levelClearSprite = gfx.sprite.new(level2ClearImage)
+		elseif currentLevel == 3 then
+			levelClearSprite = gfx.sprite.new(level3ClearImage)
+		elseif currentLevel == 4 then
+			levelClearSprite = gfx.sprite.new(level4ClearImage)
+		elseif currentLevel == 5 then
+			levelClearSprite = gfx.sprite.new(level5ClearImage)
+		elseif currentLevel == 6 then
+			levelClearSprite = gfx.sprite.new(level6ClearImage)
+		elseif currentLevel == 7 then
+			levelClearSprite = gfx.sprite.new(level7ClearImage)
+		elseif currentLevel == 8 then
+			levelClearSprite = gfx.sprite.new(level8ClearImage)
+		end
+	else
+		if currentLevel == 1 then
+			levelClearSprite = gfx.sprite.new(level1ClearGentleImage)
+		elseif currentLevel == 2 then
+			levelClearSprite = gfx.sprite.new(level2ClearGentleImage)
+		elseif currentLevel == 3 then
+			levelClearSprite = gfx.sprite.new(level3ClearGentleImage)
+		elseif currentLevel == 4 then
+			levelClearSprite = gfx.sprite.new(level4ClearGentleImage)
+		elseif currentLevel == 5 then
+			levelClearSprite = gfx.sprite.new(level5ClearGentleImage)
+		elseif currentLevel == 6 then
+			levelClearSprite = gfx.sprite.new(level6ClearGentleImage)
+		elseif currentLevel == 7 then
+			levelClearSprite = gfx.sprite.new(level7ClearGentleImage)
+		elseif currentLevel == 8 then
+			levelClearSprite = gfx.sprite.new(level8ClearGentleImage)
+		end
 	end
 
 	levelClearSprite:moveTo(200, 120)
