ddfa6838a2bf680c3aa4424653968af54afdac97
Steve Cochrane <steve@stevecochrane.com>
2023-11-03 22:25:31 -0500

    Update Credits messaging

--- source/creditsText.lua
+++ source/creditsText.lua
@@ -17,22 +17,23 @@ function CreditsText:draw()
 
 	gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
 
-	gfx.drawText("For Darya, with love", margin, margin)
+	gfx.setFont(Roobert10Bold)
+	gfx.drawText("Design, Programming, Graphics", margin, margin)
+	gfx.setFont(Roobert11Medium)
+	gfx.drawText("Steve Cochrane", margin, margin + rowHeight * 1)
 
 	gfx.setFont(Roobert10Bold)
-	gfx.drawText("Design, Programming, Graphics", margin, margin + rowHeight * 2)
+	gfx.drawText("Music, Sound Effects", margin, margin + rowHeight * 3)
 	gfx.setFont(Roobert11Medium)
-	gfx.drawText("Steve Cochrane", margin, margin + rowHeight * 3)
+	gfx.drawText("KRVB", margin, margin + rowHeight * 4)
 
 	gfx.setFont(Roobert10Bold)
-	gfx.drawText("Music, Sound Effects", margin, margin + rowHeight * 5)
+	gfx.drawText("Playtesting, Feedback", margin, margin + rowHeight * 6)
 	gfx.setFont(Roobert11Medium)
-	gfx.drawText("KRVB", margin, margin + rowHeight * 6)
+	gfx.drawText("Darya, Maman", margin, margin + rowHeight * 7)
 
-	gfx.setFont(Roobert10Bold)
-	gfx.drawText("Playtesting, Feedback", margin, margin + rowHeight * 8)
 	gfx.setFont(Roobert11Medium)
-	gfx.drawText("Darya, Darya's Maman", margin, margin + rowHeight * 9)
+	gfx.drawText("Thank you for playing!", margin, margin + rowHeight * 10)
 
 	gfx.drawTextAligned("Ⓐ", 390, margin + rowHeight * 10, kTextAlignment.right)
 
